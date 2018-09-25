fdb4241b4e7a257ab642ef45affd3159ff0ae1eb
Jonny Adams <adams.jonathanpaul@gmail.com>
2018-09-25 15:30:47 +0100

    Added second chart for SU growth

--- scripts/initial analysis.R
+++ scripts/initial analysis.R
@@ -14,8 +14,15 @@ lads <- read.csv(lads_path)
 lads_trim <- filter(lads, sutotal_per100k2016 < 100)
 
 #Draw graph
-svg(filename = "Scaleup beeswarm.png")
+svg(filename = "Scaleup density beeswarm.svg")
 
 beeswarm(lads_trim$sutotal_per100k2016, method="swarm", col="#247BA0", pch=16)
+
+dev.off()
+
+#Draw graph
+svg(filename = "Scaleup growth beeswarm.svg")
+
+beeswarm(lads_trim$sutotal_growth13_16, method="swarm", col="#247BA0", pch=16)
 
 dev.off()
