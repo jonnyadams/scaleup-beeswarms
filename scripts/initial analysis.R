7789929ae36be668bfd720dffac1c4fc43bc1525
Jonny Adams <adams.jonathanpaul@gmail.com>
2018-09-28 10:22:09 +0100

    Added new charts, and changed the swarm into hex.

--- scripts/initial analysis.R
+++ scripts/initial analysis.R
@@ -14,15 +14,38 @@ lads <- read.csv(lads_path)
 lads_trim <- filter(lads, sutotal_per100k2016 < 100)
 
 #Draw graph
-svg(filename = "Scaleup density beeswarm.svg")
+svg(filename = "Scaleup density beeswarm (total).svg")
 
-beeswarm(lads_trim$sutotal_per100k2016, method="swarm", col="#247BA0", pch=16)
+beeswarm(lads_trim$sutotal_per100k2016, method="hex", col="#247BA0", pch=16, ylim=c(0,85))
 
 dev.off()
 
 #Draw graph
-svg(filename = "Scaleup growth beeswarm.svg")
+svg(filename = "Scaleup density beeswarm (emp).svg")
 
-beeswarm(lads_trim$sutotal_growth13_16, method="swarm", col="#247BA0", pch=16)
+beeswarm(lads_trim$suemp_per100k2016, method="hex", col="#247BA0", pch=16, ylim=c(0,85))
 
 dev.off()
+
+#Draw graph
+svg(filename = "Scaleup density beeswarm (tur).svg")
+
+beeswarm(lads_trim$sutur_per100k2016, method="hex", col="#247BA0", pch=16, ylim=c(0,85))
+
+dev.off()
+
+#Draw graph
+svg(filename = "Scaleup growth beeswarm (emp).svg")
+
+beeswarm(lads_trim$suemp_growth13_16, method="swarm", col="#247BA0", pch=16)
+
+dev.off()
+
+
+#Draw graph
+svg(filename = "Scaleup growth beeswarm (tur).svg")
+
+beeswarm(lads_trim$sutur_growth13_16, method="hex", col="#247BA0", pch=16)
+
+dev.off()
+
